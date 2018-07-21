57611cc35c10a49b28e258edfb5a26260df02fe5
Tyler <tyler.stirtz@gmail.com>
2018-07-21 16:13:29 -0700

    Remove server conditional

--- server.go
+++ server.go
@@ -7,7 +7,5 @@ import (
 func main() {
 	port := "8080"
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		panic(err)
-	}
+	http.ListenAndServe(":"+port, nil)
 }
